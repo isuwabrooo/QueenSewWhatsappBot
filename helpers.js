374e71b0c06344b8199a5d15e18e97f9eb5649b6
King isuwa <89562017+isuwabrooo@users.noreply.github.com>
2021-12-19 10:15:51 +0530

    Add files via upload

---
+++ helpers.js
@@ -0,0 +1,28 @@
+/* Codded by @isuwabrooo
+
+Telegram: t.me/isuwabrooo
+Facebook: https://www.facebook.com/isuwa.brooo.79
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+Whats bot - isuwabrooo
+
+*/
+
+function successfullMessage(msg) {
+    return "🇱🇰 *Sew Queen👑*:  ```" + msg + "```"
+}
+function errorMessage(msg) {
+    return "🥴 *Sew Queen👑*:  ```" + msg + "```"
+}
+function infoMessage(msg) {
+    return "❄ *Sew Queen👑*:  ```" + msg + "```"
+}
+
+
+module.exports = {
+    successfullMessage,
+    errorMessage,
+    infoMessage
+}
